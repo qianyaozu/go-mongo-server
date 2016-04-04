7699e5481a80315ead357b71ba8576ff5aec6643
qianyaozu <262236468@qq.com>
2016-04-04 14:42:10 +0800

    common  mongodb server

--- util.go
+++ util.go
@@ -1,5 +1,10 @@
 package main
-import "log"
+import (
+"log"
+"fmt"
+"os"
+"time"
+)
 //公共方法
 //集合中是否包含这个数据
 func  ContainKey(key interface{} ,arr []interface{}) bool {
@@ -23,7 +28,7 @@ func  ContainStringKey(key string ,arr []string) bool {
 //检查错误
 func CheckErr(err error, msg string) bool {
 	if err != nil {
-		log.Println(msg, err)
+		Log(msg, err)
 		return true
 	}
 	return false
@@ -32,8 +37,35 @@ func CheckErr(err error, msg string) bool {
 //错误恢复
 func Recovery(title string) {
 	if err := recover(); err != nil {
-		log.Println(title, "->", err)
+		Log(title,   err)
 	}
 }
 
+//日志系统
+func Log(title string,data interface{}) {
+	dir := "log/"
+	if _, err := os.Stat(dir); err != nil {
+		err = os.MkdirAll(dir, 0777)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	fileName := dir + "/" + time.Now().Format("20060102") + ".log"
+	logFile, err := os.OpenFile(fileName, os.O_CREATE | os.O_APPEND, 0666)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+		return
+	}
+	debugLog := log.New(logFile, "[Debug]", log.LstdFlags)
+	if title != "" {
+		debugLog.Println(title)
+		fmt.Println(title)
+	}
+	if data != nil {
+		debugLog.Println(data)
+		fmt.Println(data)
+	}
+}
 
